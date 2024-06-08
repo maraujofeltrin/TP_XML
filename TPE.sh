01cb51596a9ff93ab0e44db42cdfa36e49dfeddb
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2024-06-08 20:53:57 -0300

    agrego carpeta fop

--- TPE.sh
+++ TPE.sh
@@ -1,10 +1,33 @@
+#!/bin/bash
+
 source cte.sh
 
 if [ $# -ne 2 ]
 then
-    ERRORES+="Error: Cantidad de parametros incorrecta\n"
+    ERRORES+="<error>Cantidad de parametros incorrecta </error>.\n"
+fi
+
+if [ -z "$ERRORES" ]
+then
+    if [ $1 -lt 2013 ] || [ $1 -gt 2024 ]
+    then 
+        year="$1" 
+        ERRORES+="<error>Año ${year} inválido.</error>\n"
+    fi 
+    if [ $2 != "sc" ] && [ $2 != "xf" ] && [ $2 != "cw" ] && [ $2 != "go" ] && [ $2 != "mc" ]
+    then
+        type="$2"
+        ERRORES+="<error>ETipo ${type} inválido.</error>\n"
+    fi
+
+    if [ -z "$ERRORES" ]
+    then 
+        echo "todo en condiciones"
+        ./fop-2.9/fop/fop -fo ./fop-2.9/fop/examples/fo/basic/simple.fo -pdf example.pdf 
+
+    fi
 fi
 
 
 
-echo "${ERRORES}"
+echo -e $ERRORES
--- cte.sh
+++ cte.sh
@@ -1,6 +1,8 @@
+ #!/bin/bash
  
  
- 
- export SPORTRADAR_API=oAachkC35R7ZjvFQGE0xg11X2dMqPLH0aAr5bc1h
+export SPORTRADAR_API=oAachkC35R7ZjvFQGE0xg11X2dMqPLH0aAr5bc1h
 
- export ERRORES=""
+export ERRORES=""
+
+
