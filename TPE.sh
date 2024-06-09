6016517bf2b20a0a0b678b4e513f791eb526a209
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2024-06-09 17:34:18 -0300

    Update TPE.sh

--- TPE.sh
+++ TPE.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+dos2unix cte.sh &> /dev/null
+dos2unix extract_nascar_data.xq &> /dev/null
+dos2unix generate_fo.xsl &> /dev/null
+dos2unix fop-2.9/fop/fop* &> /dev/null
+dos2unix fop-2.9/fop* &> /dev/null
+dos2unix fop-2.9/* &> /dev/null
+
 source cte.sh
+
+
+
+
 
 if [ $# -ne 2 ]
 then
@@ -13,7 +24,6 @@ then
     type="$2"
     if [ $1 -lt 2013 ] || [ $1 -gt 2024 ]
     then 
-        
         ERRORES=2     #"<error>Año ${year} inválido.</error>\n"
     fi 
     if [ $2 != "sc" ] && [ $2 != "xf" ] && [ $2 != "cw" ] && [ $2 != "go" ] && [ $2 != "mc" ]
